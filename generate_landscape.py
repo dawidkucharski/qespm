94b5f6d544f2cfe25bb874d211fd32814e72b63d
Dawid Kucharski <dawid.kucharski@put.poznan.pl>
2026-09-17 09:37:58 +0200

    Move SEM label right to avoid overlap with optical profilometry label

--- generate_landscape.py
+++ generate_landscape.py
@@ -32,7 +32,7 @@ for name, m in methods.items():
         'AFM (tapping)':        (1.8, 1.8),
         'STM':                  (1.5, 2.5),
         'Optical profilometry':  (0.25, 0.5),
-        'SEM':                  (2.5, 0.5),
+        'SEM':                  (6.0, 1.2),
         r'$\mathbf{QESPM}$':    (0.5, 4.0),
     }
     mx, my = offsets.get(name, (1.5, 1.5))
